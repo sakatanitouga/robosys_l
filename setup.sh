df3b266ffe04ba527d893783e3b32840decd3be5
sakatanitouga <sakatani.touga@gmail.com>
2021-01-05 01:03:25 +0900

    Edit

---
+++ setup.sh
@@ -0,0 +1,2 @@
+curl -kL https://bootstrap.pypa.io/get-pip.py | python
+pip install "pygame==1.9.6"
--- src/tkinter_ui.py
+++ src/tkinter_ui.py
@@ -6,11 +6,16 @@ from std_msgs.msg import Int8
 import sys
 import time
 import random
+import pygame
+
+
+
 #
 # GUI設定
 #
 Endtime = 20
 End = False
+Start = False
 
 rospy.init_node('tkinterUI', anonymous=True)
 x=150
@@ -26,13 +31,15 @@ m_size_y = 15
 
 
 root = Tkinter.Tk()
-root.title("雨粒集めてゲーム")
+root.title("Rainy")
 root.geometry("800x450")
+
 misile=[]
 misile_x = []
 misile_y = []
 misile_fire = []
 misile_size_list = []
+misile_hit_list = []
 
 misile_v = 7
 #ミサイルの個数
@@ -51,12 +58,25 @@ canvas = Tkinter.Canvas(root, width = 800, height = 450,bg="white")#Canvasの作
 canvas.pack()#Canvasの配置
 square = canvas.create_rectangle(5, 5, 5+dx, 5+dy, fill = 'black')#塗りつぶし
 
+
 for count in range(misile_cnt):
     misile_x.append(count*m_e)
     misile_y.append(-30)
     misile_size_list.append(m_size_y*random.randint(1,3))
     misile.append(canvas.create_rectangle(misile_x[count], misile_y[count], misile_x[count] + m_size_x, misile_y[count] +misile_size_list[count], fill = 'black')) 
     misile_fire.append(False)
+    misile_hit_list.append(False)
+
+print("start!")
+
+pyg = pygame
+pyg.mixer.init()
+def sound(flie_name):
+    global pyg
+    pygame.mixer.music.stop()
+    pyg.mixer.music.load(flie_name)
+    pyg.mixer.music.play(1)
+
 
 def misile_maneger():
     for count in range(misile_cnt):
@@ -64,10 +84,14 @@ def misile_maneger():
             fire_misile(count)
     
 def fire_misile(num):
-    global misile,misile_x,misile_y,misile_fire,y,dy,x,dx,score
+    global misile,misile_x,misile_y,misile_fire,misile_hit_list,y,dy,x,dx,score
     misile_x[num] += 0
     if y <= misile_y[num]+m_size_y and y+dy >= misile_y[num]:    
         if x <= misile_x[num]+m_size_x and x+dx >= misile_x[num]:
+            if not misile_hit_list[num]:
+                misile_hit_list[num] = True
+                sound("sound1.mp3")
+
             score+=1
             
     if misile_y[num]<500:
@@ -78,13 +102,15 @@ def fire_misile(num):
         misile_fire[num] = False
     canvas.coords(misile[num],misile_x[num],misile_y[num],misile_x[num]+m_size_x,misile_y[num]+misile_size_list[num])
 
+
 def callback(num):
-    global x,y,dx,dy,cnt,score,Endtime,canvas,End
+    global x,y,dx,dy,cnt,score,Endtime,canvas,End,Start
+    
     if cnt <50*Endtime:
         cnt += 1
         text.set("score:"+str(score)+"  time:"+str(Endtime-cnt/50))
         canvas.coords(square,x,y,x+dx,y+dy)
-        x += 10*num.data
+        x -= 10*num.data
         misile_maneger()
         if cnt % 20 == 0:
             num = random.randint(0,misile_cnt-1)
@@ -93,10 +119,11 @@ def callback(num):
     else:
         if not End:
             text.set("!!END!!")
+            sound("sound2.mp3")
             canvas.create_rectangle(0,0,800,450,fill = 'white',)
             canvas.create_text(400, 100, text = "score:"+str(score), font = ('MSゴシック', 100), fill = 'black')
             End = True
     root.update()
-print("start!")
+
 rospy.Subscriber("/teleop", Int8, callback)
 root.mainloop()       
